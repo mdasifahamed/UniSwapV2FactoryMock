7ad4d20dca99a43e7501720b95737455bd1ecceb
mdasifahamed <mdasifahamed89@yahoo.com>
2024-10-28 16:50:19 +0600

    name resolve

--- scripts/interact/CreatePair.js
+++ scripts/interact/CreatePair.js
@@ -3,13 +3,13 @@ const getWallet = require("../wallet");
 const factoryAddress = "0xe27eD3412469eD2C1FAD33d5B053Afc866a37634";
 
 const { getContract } = require("../getContract");
-async function GetALLPairs() {
+async function CreatePair() {
   const wallet = await getWallet(process.env.token_pair_creator);
   const { contract } = await getContract(factoryAddress);
   const DAIToken = "0x97E37850D1050ee041dA4b7451DCd0d57fcE27Cc";
-  const WETH = "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9";
+  const MAKER = "0x3d83AD8F864B8Cc50723A2d10d3a013b164E2d14";
   try {
-    const tx = await contract.connect(wallet).createPair(DAIToken, WETH);
+    const tx = await contract.connect(wallet).createPair(DAIToken, MAKER);
     await tx.wait();
     console.log(tx);
   } catch (error) {
@@ -17,4 +17,4 @@ async function GetALLPairs() {
   }
 }
 
-GetALLPairs();
+CreatePair();
